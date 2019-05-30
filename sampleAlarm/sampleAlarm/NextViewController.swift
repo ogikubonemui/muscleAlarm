38021961487d16567372da95584654e34a043a3f
yasuyukitakeshita <thetake1206@gmail.com>
2019-05-30 21:09:54 +0800

    Revert "[fix]作業ファイルを「sampleAlarm」へ変更"
    This reverts commit a8f6f8f7323cf8ed922ade06db826d560eaee709.

--- sampleAlarm/sampleAlarm/NextViewController.swift
+++ sampleAlarm/sampleAlarm/NextViewController.swift
@@ -7,14 +7,10 @@ class NextViewController: UIViewController {
 
     var audioPlayer: AVAudioPlayer!
     
-    
-    @IBOutlet weak var label: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 画面を読み込むと同時に音楽が再生される
         playMusic()
-        label.text = "画面遷移完了"
     }
     
     @IBAction func didTapBtn(_ sender: Any) {
